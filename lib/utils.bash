1d78bfb82c1f9af9f38902cfec70ddf493c2fd9d
Andreas Steffan <a.steffan@contentreich.de>
2022-10-09 12:45:05 +0200

    wip

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for clusterawsadm.
 GH_REPO="https://github.com/kubernetes-sigs/cluster-api-provider-aws"
 TOOL_NAME="clusterawsadm"
 TOOL_TEST="clusterawsadm --help"
@@ -19,6 +18,23 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
+# get_arch discovers the architecture for this system .
+get_arch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+  armv5*) ARCH="armv5" ;;
+  armv6*) ARCH="armv6" ;;
+  armv7*) ARCH="arm" ;;
+  arm64) ARCH="arm64" ;; # TODO Fix to proper when M1 packages are available
+  aarch64) ARCH="arm64" ;;
+  x86) ARCH="386" ;;
+  x86_64) ARCH="amd64" ;;
+  i686) ARCH="386" ;;
+  i386) ARCH="386" ;;
+  esac
+  echo "$ARCH"
+}
+
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
@@ -31,8 +47,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if clusterawsadm has other means of determining installable versions.
   list_github_tags
 }
 
@@ -41,8 +55,11 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for clusterawsadm
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  local operating_system="$(uname | tr '[:upper:]' '[:lower:]')"
+  local arch="$(get_arch)"
+  local platform="${operating_system}-${arch}"
+
+  url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-${platform}"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
